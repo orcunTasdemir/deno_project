fed4f22b80de3466942c0f1f8e36657be54a00e6
orcunTasdemir <orcntasdemir@gmail.com>
2023-09-08 01:26:00 -0500

    linted and KV path changed

--- utils/db.ts
+++ utils/db.ts
@@ -2,14 +2,14 @@
 import { assertNotEquals } from "std/testing/asserts.ts";
 import { chunk } from "std/collections/chunk.ts";
 
-//const KV_PATH_KEY = "KV_PATH";
-const path = "./DB/db";
-// if (
-//   (await Deno.permissions.query({ name: "env", variable: KV_PATH_KEY }))
-//     .state === "granted"
-// ) {
-//   path = Deno.env.get(KV_PATH_KEY);
-// }
+const KV_PATH_KEY = "KV_PATH";
+let path = undefined;
+if (
+  (await Deno.permissions.query({ name: "env", variable: KV_PATH_KEY }))
+    .state === "granted"
+) {
+  path = Deno.env.get(KV_PATH_KEY);
+}
 export const kv = await Deno.openKv(path);
 
 // Helpers
